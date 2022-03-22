b56559553764e8e87e297642c133c41d26612e51
Gulshat <gubgg@yandex.ru>
2022-03-22 10:56:49 +0500

    .

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Список задач</h1>
+    <AddToDo />
+    <ToDoList v-bind:arrayTasks="arrayTasks" @delete-task="deleteTask" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from "./components/ToDoList";
+import AddToDo from "./components/AddToDo";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      arrayTasks: [
+        {
+          id: 1,
+          title: "title1",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "title2",
+          completed: false,
+        },
+      ],
+    };
+  },
+  components: { ToDoList, AddToDo },
+  methods: {
+    deleteTask(id) {
+      this.arrayTasks = this.arrayTasks.filter((t) => t.id != id);
+    },
+  },
+};
 </script>
 
 <style>
+body {
+  background-color: #f0f0f0;
+  height: 100vh;
+  width: 100%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  margin: 120px auto;
+  max-width: 400px;
+  width: 100%;
+  background: #918e94;
+  border-radius: 5px;
+  padding: 25px;
+  color: #33353d;
+  font-size: 18px;
+}
+#app h1 {
+  font-size: 30px;
+  font-weight: 600;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul>
+      <ToDoItem
+        v-for="(task, index) in arrayTasks"
+        :key="index"
+        v-bind:task="task"
+        @delete-task="deleteTask"
+      />
+    </ul>
+  </div>
+</template>
+<script>
+import ToDoItem from "@/components/ToDoItem";
+export default {
+  props: ["arrayTasks"],
+  components: {
+    ToDoItem,
+  },
+  methods: {
+    deleteTask(id) {
+      this.$emit("delete-task", id);
+      console.log(id);
+    },
+  },
+};
+</script>
+
+<style>
+ul {
+  padding-left: 0;
+  list-style: none;
+}
+</style>
